0d9951dbed47f5dfa08b1756dd2351f1da71bd83
enjyo <ryo19y@gmail.com>
2019-01-27 23:24:34 +0900

    delete local_settings.py and rewrite something

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b39%#53q0nvng8x=yybjn-y#9dd@zd+5*8cnsmeb567o5fxp5&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,11 +128,7 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT ='staticfiles'
-
-DEBUG = False
+STATIC_ROOT ='os.path.join(BASE_DIR, 'static')'
 
 try:
     from .local_settings import *
